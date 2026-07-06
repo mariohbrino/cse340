b50680b7ae9681536af0fb65b5e5726424e6f7f6
Mario Brino <mariobrino@gmail.com>
2026-07-06 17:26:39 -0600

    fix: update SSL configuration to use environment variable for SSL setting

--- src/models/db.js
+++ src/models/db.js
@@ -14,7 +14,7 @@ import { Pool } from "pg";
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false,
+    ssl: process.env.ENABLE_SSL || true,
   },
 });
 
