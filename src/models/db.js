6aacdb4654cb0685460c79fe86b178154afb78e4
Mario Brino <mariobrino@gmail.com>
2026-07-06 21:21:14 -0600

    fix: correct SSL configuration to properly set rejectUnauthorized based on environment

--- src/models/db.js
+++ src/models/db.js
@@ -16,7 +16,7 @@ const isProduction = process.env.NODE_ENV === "production";
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    ssl: isProduction ? { rejectUnauthorized: false } : false,
+    ssl: isProduction ? false : { rejectUnauthorized: false },
   },
 });
 
