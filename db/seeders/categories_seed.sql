eefb172eaa34dacb7a98509019e67aa2d465c95c
Mario Brino <mariobrino@gmail.com>
2026-07-08 16:09:36 -0600

    refactor: update sql queries

--- db/seeders/categories_seed.sql
+++ db/seeders/categories_seed.sql
@@ -1,9 +1,15 @@
-INSERT INTO category (name) VALUES
-('Infrastructure & Construction'),
-('Renewable Energy & Sustainability'),
-('Community Spaces & Recreation'),
-('Urban Farming & Food Security'),
-('Education & Youth Development'),
-('Social Services & Support'),
-('Transportation & Mobility'),
-('Environmental Cleanup & Beautification');
+INSERT INTO category (name) VALUES ('Infrastructure & Construction');
+
+INSERT INTO category (name) VALUES ('Renewable Energy & Sustainability');
+
+INSERT INTO category (name) VALUES ('Community Spaces & Recreation');
+
+INSERT INTO category (name) VALUES ('Urban Farming & Food Security');
+
+INSERT INTO category (name) VALUES ('Education & Youth Development');
+
+INSERT INTO category (name) VALUES ('Social Services & Support');
+
+INSERT INTO category (name) VALUES ('Transportation & Mobility');
+
+INSERT INTO category (name) VALUES ('Environmental Cleanup & Beautification');
