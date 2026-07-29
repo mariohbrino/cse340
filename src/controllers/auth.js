2c01162749565d512c511308a581e91999ab8729
Mario Brino <mariobrino@gmail.com>
2026-07-29 15:26:49 -0600

    fix: remove password length validation from authValidation

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -42,7 +42,6 @@ const authValidation = [
   body("password")
     .notEmpty()
     .withMessage("Password is required")
-    .isLength({ min: 8 })
     .withMessage("Password must be at least 8 characters long"),
 ];
 
